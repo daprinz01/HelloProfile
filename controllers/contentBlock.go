d65c609e13b05b6da9db52d2696cee28dcc7ed77
Prince Okechukwu <prince.okechukwu@kudabank.com>
2022-02-19 18:15:50 +0100

    Fix create content block error

--- controllers/contentBlock.go
+++ controllers/contentBlock.go
@@ -56,6 +56,7 @@ func (env *Env) AddContentBlock(c echo.Context) (err error) {
 		dbContent.Order = request.Order
 		dbContent.Title = request.Title
 		dbContent.Url = request.Url
+		dbContent.ProfileID = profileId
 		dbAddContentResult, err := env.HelloProfileDb.AddProfileContent(context.Background(), *dbContent)
 		if err != nil {
 			errorResponse.Errorcode = util.SQL_ERROR_CODE
